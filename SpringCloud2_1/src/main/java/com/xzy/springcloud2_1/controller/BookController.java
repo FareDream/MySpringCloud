133bee61b5a76bd7ae45d52fcde8e74fef7d807e
Fare丶梦 <1269131995@qq.com>
2018-02-26 10:40:17 +0800

    Add files via upload

---
+++ SpringCloud2_1/src/main/java/com/xzy/springcloud2_1/controller/BookController.java
@@ -0,0 +1,11 @@
+package com.xzy.springcloud2_1.controller;
+
+/**
+ * @author 项目作者：XuZiyu
+ * @project 项目名称：SpringCloud2_1
+ * @package 包名：com.xzy.springcloud2_1.controller
+ * @data 创建时间：2018-2-24 17:22
+ * @description 描述：
+ */
+public class BookController {
+}
